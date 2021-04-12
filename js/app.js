0e1bafd1efc3358b829174779464c35a1996bd16
ahmadhomsi <homsiahmad111@gmail.com>
2021-04-12 22:28:40 +0300

    new file js

---
+++ js/app.js
@@ -0,0 +1,69 @@
+'use strict'
+alert("welcome  in my bage , we will play fun game called guessing game ");
+let userName=prompt("what is your name ?");
+console.log(userName);
+
+alert("Hello " +userName+ "this game will be about me ,lets start");
+
+let fName = prompt("is my name ahmd ? pleas answer with yes or no ").toLowerCase();
+if (fName=="yes"){
+    alert("good answer");
+}
+else {
+     alert("sorry it is wrong answer");
+    }
+let cars = prompt("can i drive  ? pleas answer with yes or no ").toLowerCase();
+if (cars=="no"){
+    alert("good answer");
+}
+else {
+     alert("sorry it is wrong answer");
+    }
+    let major = prompt("is english langage my studies  ? pleas answer with yes or no ").toLowerCase();
+if (major=="yes"){
+    alert("good answer");
+}
+else {
+     alert("sorry it is wrong answer");
+    }
+
+    let age = prompt("is my age 29 ? pleas answer with yes or no ").toLowerCase();
+if (cars=="yes"){
+    alert("good answer");
+}
+else {
+     alert("sorry it is wrong answer");
+    }
+
+    let experens = prompt("did i have 5 years ex ? pleas answer with yes or no ").toLowerCase();
+if (cars=="yes"){
+    alert("good answer");
+}
+else {
+     alert("sorry it is wrong answer");
+    }
+
+    alert("Hello " +userName+ "this game was fun and nice to meet you");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
